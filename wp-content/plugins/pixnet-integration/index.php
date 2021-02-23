1427a7afb582043eb5b80f2bf68e1730eaf8cd0b
yuehnanwu <joram19910115@gmail.com>
2021-02-23 17:59:26 +0800

    Setup pixnet sdk

---
+++ wp-content/plugins/pixnet-integration/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Plugin Name: Pixnet integration
+ * Plugin URI: https://nanforce.com
+ * Description: For the custom wordpress plugin which is to integrate with pixnet.com
+ * Version: 1.0
+ * Author: Yuehnan, Wu
+ * Author URI: https://nanforce.com
+ */
+require_once(__DIR__ . '/src/PIXNET/Loader.php');
+
+$pixapi = new PixAPI(array(
+    'key'  => '1e547b8651056884c27da3b5f635f831',
+    'secret' => 'a7982792fec1e47fa49abc18fc789ef6',
+    'callback' => 'https://nanforce.com/wp-json/pixet/v1/auth-callback'
+));
+
+add_action('rest_api_init', 'register_routes');
+
+function register_routes()
+{
+    register_rest_route('pixnet/v1', '/access', array(
+        'methods' => 'GET',
+        'callback' => 'get_is_access',
+    ));
+    register_rest_route('pixnet/v1', '/auth-callback', array(
+        'methods' => 'GET',
+        'callback' => 'get_is_access',
+    ));
+}
+
+
+function get_is_access()
+{
+    $post =
+        ['status_code' => 200, 'msg' => 'Access api "pixnet: v1" successfully'];
+    return $post;
+}
+
+function get_pixet_auth_callback()
+{
+    return 
+}
